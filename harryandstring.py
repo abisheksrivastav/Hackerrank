ce566f9f527cc167994a61e5da5e4bf5cdcbe476
abishek <abisheksrivastav@gmail.com>
2021-12-23 16:13:21 +0530

    commit on master

--- harryandstring.py
+++ harryandstring.py
@@ -4,4 +4,23 @@
 # All the other spells that do not fall in any of the categories before are categorized as Bad
 
 
-  
+k = int(input().strip())
+p = "aeiou"
+for _ in range(k):
+    s = input().strip()
+    v = []
+    for i in s:
+        if i in p:
+            v.append(i)
+    r = []
+    for k in p:
+        for j in v:
+            if j == k:
+                r.append(k)
+    if r == v:
+        print("Good")
+    elif  v==r[-1::-1]:
+        print("Worst")
+    else:
+        print("Bad")       
+    
